79fac28fcd4b04866ce405325df5e3b106c9640a
Hitesh <soneta.h@northeastern.edu>
2023-12-23 22:17:27 -0500

    final fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import r from "../Navbar/Resume.pdf"
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
+import { saveAs } from 'file-saver';
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [show,setShow]=useState(false);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    if (window.innerWidth < 950) {
+      setShow(true);
+    }
+    else{
+      setShow(false)
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const downloadResume = () => {
+    const resumeFilePath = r; // Update with your actual file name and extension
+    saveAs(resumeFilePath, 'Resume.pdf');
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -12,7 +37,7 @@ export const Navbar = () => {
         Portfolio
       </a>
       <div className={styles.menu}>
-        <img
+        {show && <img
           className={styles.menuBtn}
           src={
             menuOpen
@@ -21,7 +46,8 @@ export const Navbar = () => {
           }
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
-        />
+        />}
+        
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
@@ -39,7 +65,7 @@ export const Navbar = () => {
             <a href="#about">Contact</a>
           </li>
           <li>
-            <a href="#about">Resume</a>
+            <a href="#" onClick={downloadResume}>Resume</a>
           </li>
         </ul>
       </div>
